17c2a6a081b462b5bb8352d27a2048b67dee214c
josephhensh <josephhensh2000@gmail.com>
2022-06-25 14:29:13 +0100

    Create post_list template, modify admin.py, update views

--- I4G008955JNO/blog/views.py
+++ I4G008955JNO/blog/views.py
@@ -3,6 +3,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 
@@ -22,7 +25,7 @@ class PostUpdateView(UpdateView):
     fields = "__all__"
     success_url = reverse_lazy("blog:all")  
 
-class PostDeleteView(UpdateView):
+class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")                 
